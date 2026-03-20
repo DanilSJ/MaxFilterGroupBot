c6c7ea5904960d94cdc31e51b7b3fa195b168e34
DanilSJ <danilchagarnoy@mail.ru>
2026-03-20 22:41:28 +0300

    create dispatcher

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from maxfiltergroupbot!")
+import asyncio
+from maxapi import Dispatcher
+from core.config import bot
+
+dp = Dispatcher()
+
+async def main():
+    dp.include_routers()
+
+    await asyncio.gather(
+        dp.start_polling(bot)
+    )
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Exit")
