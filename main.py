4bfe189973e5594bec85f3475270dc5967d9326c
DanilSJ <danilchagarnoy@mail.ru>
2026-03-22 23:29:21 +0300

    include start_router

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import asyncio
 from maxapi import Dispatcher
 from core.config import bot
+from app.start.headers import router as start_router
 
 dp = Dispatcher()
 
 async def main():
-    dp.include_routers()
+    dp.include_routers(start_router)
 
     await asyncio.gather(
         dp.start_polling(bot)
