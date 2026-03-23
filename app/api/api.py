a8662be6649ba2c46b4f5434599b546a54d8c96b
DanilSJ <danilchagarnoy@mail.ru>
2026-03-23 22:43:54 +0300

    new api url

--- app/api/api.py
+++ app/api/api.py
@@ -4,5 +4,5 @@ from core.config import settings
 
 async def get_group(group_id: int):
     async with httpx.AsyncClient() as client:
-        r = await client.get(f"{settings.API_URL}/group/{group_id}/")
+        r = await client.get(f"{settings.API_URL}/group/max/{group_id}/")
         return r
