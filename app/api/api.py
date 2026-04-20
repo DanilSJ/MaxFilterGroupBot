f48add293233b204f23201eb25be148b84c29759
DanilSJ <danilchagarnoy@mail.ru>
2026-04-20 22:30:44 +0300

    create create_account

--- app/api/api.py
+++ app/api/api.py
@@ -6,3 +6,9 @@ async def get_group(group_id: int):
     async with httpx.AsyncClient() as client:
         r = await client.get(f"{settings.API_URL}/group/max/{group_id}/")
         return r
+
+
+async def create_account(max_id: int):
+    async with httpx.AsyncClient() as client:
+        r = await client.post(f"{settings.API_URL}/user/max/", json={"max_id": max_id})
+        return r
