cbf5d6b47c95e31c268c09dcb7b51cb547b0ad08
DanilSJ <danilchagarnoy@mail.ru>
2026-04-23 14:06:51 +0300

    add try except

--- app/api/api.py
+++ app/api/api.py
@@ -4,11 +4,17 @@ from core.config import settings
 
 async def get_group(group_id: int):
     async with httpx.AsyncClient() as client:
-        r = await client.get(f"{settings.API_URL}/group/max/{group_id}/")
-        return r
+        try:
+            r = await client.get(f"{settings.API_URL}/group/max/{group_id}/")
+            return r
+        except Exception as e:
+            print(e)
 
 
 async def create_account(max_id: int):
     async with httpx.AsyncClient() as client:
-        r = await client.post(f"{settings.API_URL}/user/max/", json={"max_id": max_id})
-        return r
+        try:
+            r = await client.post(f"{settings.API_URL}/user/max/", json={"max_id": max_id})
+            return r
+        except Exception as e:
+            print(e)
